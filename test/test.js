d32f04402da3b7348d6b16e3dfebd2d6044f6dc0
Haribalajiravi <haribalajiravi@gmail.com>
2020-01-20 16:44:03 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ const copy = require('./../utils/copy');
 var expect = require('chai').expect;
 var should = require('chai').should();
 
-it('Src Folder creation', function() {
+it('Project Folder Creation', function() {
     var isValid = true;
     copy('./project-generator/creamie-project', './test/test_junks');
     fs.renameSync(`./test/test_junks/creamie-project/`, `./test/test_junks/MyTest/`, (err) => {
         if (err) {
-            console.error(FgRed, err);
+            console.error(err);
         }
-        console.log(FgGreen, `Project Generated!`);
+        console.log(`Project Generated!`);
     });
     expect(isValid).to.be.true;
 });
